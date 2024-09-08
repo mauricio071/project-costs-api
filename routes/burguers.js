5a66011bd7d3271e663ca18080de1521d1354c77
Maurício Naoki <naokimau@gmail.com>
2024-09-08 18:37:25 -0300

    fix mongoose

--- routes/burguers.js
+++ routes/burguers.js
@@ -1,6 +1,7 @@
-const express = require("express")
-const mongoose = require('mongoose')
+const express = require("express");
+const connectToDatabase = require('../db');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const Burguer = mongoose.model('Burguer', {
     nome: String,
@@ -8,27 +9,30 @@ const Burguer = mongoose.model('Burguer', {
     carne: String,
     opcionais: Array,
     status: String
-})
+});
 
 router.get("/", async (req, res) => {
-    const burguers = await Burguer.find()
-    return res.send(burguers)
-})
+    await connectToDatabase();
+    const burguers = await Burguer.find();
+    return res.send(burguers);
+});
 
 router.post("/", async (req, res) => {
+    await connectToDatabase();
     const burguer = new Burguer({
         nome: req.body.nome,
         pao: req.body.pao,
         carne: req.body.carne,
         opcionais: req.body.opcionais,
         status: req.body.status
-    })
+    });
 
-    await burguer.save()
-    return res.send(burguer)
-})
+    await burguer.save();
+    return res.send(burguer);
+});
 
 router.put("/:id", async (req, res) => {
+    await connectToDatabase();
     const burguer = await Burguer.findByIdAndUpdate(req.params.id, {
         nome: req.body.nome,
         pao: req.body.pao,
@@ -37,14 +41,15 @@ router.put("/:id", async (req, res) => {
         status: req.body.status
     }, {
         new: true
-    })
+    });
 
-    return res.send(burguer)
-})
+    return res.send(burguer);
+});
 
 router.delete("/:id", async (req, res) => {
-    const burguer = await Burguer.findByIdAndDelete(req.params.id)
-    return res.send(burguer)
-})
+    await connectToDatabase();
+    const burguer = await Burguer.findByIdAndDelete(req.params.id);
+    return res.send(burguer);
+});
 
 module.exports = router;
