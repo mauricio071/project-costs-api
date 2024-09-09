a3bbd4976979d27ac24dde6855ec7357324a7425
Maurício Naoki <naokimau@gmail.com>
2024-09-08 23:20:55 -0300

    adição do cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
 
 const burguers = require('./routes/burguers')
 const ingredientes = require("./routes/ingredientes")
 const status = require("./routes/status")
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/ingredientes', ingredientes);
